b3622c88e21619042351686e45ee9249edcc356a
sachin jayakody <Sachi.jayakody98@gmail.com>
2024-10-14 20:33:09 +1100

    add docker and deployment file

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,8 @@
-# Dockerfile for book_catalog microservice
+FROM python:3.10-slim
+ 
+WORKDIR /usr/src/app/book_catalog
+COPY book_catalog/requirements.txt .
+RUN pip install --no-cache-dir -r requirements.txt
+COPY book_catalog/ .
+EXPOSE 8000
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- book_catalog/config.py
+++ book_catalog/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Settings:
-    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    DATABASE_URL: str = os.getenv("DATABASE_URL","postgresql://my_database_uzi7_user:UwqBGbe9bInNNvFJidWMS4mySZrzOjCm@dpg-cs6desrtq21c73dqe4mg-a.singapore-postgres.render.com/my_database_uzi7")
 
 settings = Settings()
